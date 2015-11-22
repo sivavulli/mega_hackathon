79cfdbf1179aeb9d4e16f2f8dbef73a907f5d8ef
nanu146 <pradeep146nanu@gmail.com>
2015-11-22 22:52:11 +0530

    Create TransactionTimeSpent.php

---
+++ TransactionTimeSpent.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+include('TransactionClass.php');
+include('ClassSessions.php');
+
+function nullif($var){
+	if(isset($var) && !empty($var))
+	{
+		return $var;
+	}
+	else {return null;}
+}
+
+$con=mysqli_connect("us-cdbr-iron-east-03.cleardb.net","b5cc7e24bc5291","4510e1e2","ad_b0866641ccff844");
+if (mysqli_connect_errno($con))
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$sql = "INSERT INTO  tbl_transaction_timespent_details".
+				"(TransactionID,Status,TimeSpent)".
+				" VALUES ('"
+				.nullif(TransactionID)."','"
+				.nullif(Status)."',".nullif(TimeSpent).")";
+
+						if (!mysqli_query($con,$sql))
+						{
+							die('Error: ' . mysqli_error($con));
+						}
+						
+						mysqli_close($con);
+
+	

cb932302fe6d944bb703ad7146afb37ae955aaf4
nanu146 <pradeep146nanu@gmail.com>
2015-11-23 03:25:45 +0530

    Update LineChartData.php

--- LineChartData.php
+++ LineChartData.php
@@ -1,11 +1,17 @@
 <?php
 
-		
+class lineclass
+{
+	
+	public $date;
+	public $close;
+}
 header("Content-type: text/tsv");
 /*header("Content-Disposition: attachment; filename=file.tsv");
 header("Pragma: no-cache");
 header("Expires: 0");*/
-
+ini_set("error_reporting", E_ALL); 
+ini_set("display_errors", 1); 
 $con=mysqli_connect("us-cdbr-iron-east-03.cleardb.net","b5cc7e24bc5291","4510e1e2","ad_b0866641ccff844");
 if (mysqli_connect_errno($con))
 {
@@ -26,7 +32,8 @@ if(isset($_GET["type"]) && !empty($_GET["type"]))
 $result = $con->query($sql);
 
 
-if (!$result && !$resultcolumn) {
+
+if (!$result ) {
 	echo "Could not successfully run query ($sql) from DB: " . $con->error;
 	exit;
 }
@@ -36,56 +43,28 @@ if (mysqli_num_rows($result) == 0) {
 	exit;
 }
 
+$holder=array();
 
-$counter=1;
-while($col = mysqli_fetch_field($result))
-{
-	if($counter !=1){
-		echo "\t";
-	}
-	echo $col->name;
-$counter++;
-
-}
-echo "\r\n";
-
-
-/*
- while($col = mysqli_fetch_field($result))
- {
- echo "<pre>";
- print_r($col);
- echo "</pre>";
- echo $col->name;
-
-
- }
- */
+ 
 // While a row of data exists, put that row in $row as an associative array
 // Note: If you're expecting just one row, no need to use a loop
 // Note: If you put extract($row); inside the following loop, you'll
 //       then create $userid, $fullname, and $userstatus
-$counter2=1;
+$i=0;
 while ($row = mysqli_fetch_array($result)) {
-	/*for($i=0;$i<sizeof($row);$i++){
-		echo($row[$i]);
-		}*/
 
 	
-	for($i=0;$i<sizeof($row)/2;$i++){
-		if($i!=0){
-			echo "\t";
-		}
-		
-		echo $row[$i];
-		
-	}
-	echo "\r\n";
+	$obj=new lineclass();
+		$obj->date=$row[0];
+		$obj->close=$row[1];
+		$holder[$i]=$obj;
+		$i++;
+	
 }
 
 
-
-
+echo json_encode($holder);
 mysqli_close($con);
-		
+
+
 ?>
